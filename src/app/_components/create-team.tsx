9863d4fbb45677c133b80695ae44c2324893a382
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-03-25 14:19:45 +0100

    Team form with shadcn

--- src/app/_components/create-team.tsx
+++ src/app/_components/create-team.tsx
@@ -1,68 +1,89 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import { api } from "@/trpc/react";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  teamName: z.string().min(3).max(50),
+  teamCode: z.string().min(3)
+})
+
+type CreateTeamInput = z.infer<typeof formSchema>
+
 export function CreateTeamForm() {
   const router = useRouter();
-  const [teamName, setTeamName] = useState<string>("");
-  const [teamCode, setTeamCode] = useState<string>("");
+
+  const form = useForm<CreateTeamInput>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      teamName: "",
+      teamCode: ""
+    },
+  })
 
   const createTeam = api.team.create.useMutation({
     onSuccess: () => {
       router.refresh();
-      setTeamCode("");
-      setTeamName("");
     },
   });
 
-  return (
-    <form
-  onSubmit={(e) => {
-    e.preventDefault();
-    createTeam.mutate({ name: teamName, code: teamCode });
-  }}
-  className="flex flex-col gap-4 max-w-md mx-auto"
->
-  <div className="flex flex-col">
-    <label htmlFor="team_name" className="text-gray-700">
-      Team Name:
-    </label>
-    <input
-      type="text"
-      id="team_name"
-      name="team_name"
-      className="form-input mt-1 border border-gray-300 rounded-md"
-      value={teamName}
-      onChange={(e) => setTeamName(e.target.value)}
-      required
-    />
-    </div>
-    <div className="flex flex-col">
-      <label htmlFor="team_code" className="text-gray-700">
-        Team Code (3 characters):
-      </label>
-      <input
-        type="text"
-        id="team_code"
-        name="team_code"
-        className="form-input mt-1 border border-gray-300 rounded-md"
-        value={teamCode}
-        onChange={(e) => setTeamCode(e.target.value)}
-        pattern="[A-Za-z]{3}"
-        title="Three letter code (uppercase or lowercase) required"
-        required
-      />
-    </div>
-    <button
-      type="submit"
-      className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-    >
-      Create Team
-    </button>
-  </form>
+  function onSubmit(values: CreateTeamInput) {
+    createTeam.mutate({
+      code: values.teamCode,
+      name: values.teamName
+    });
+  }
 
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="teamName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Team Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Team Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="teamCode"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Team Code</FormLabel>
+              <FormControl>
+                <Input placeholder="Team Code" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
