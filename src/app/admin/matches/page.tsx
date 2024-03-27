4b25c67a7799a5c8cf5b35be2fd8c4240de037f7
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-03-27 21:59:15 +0100

    styling

--- src/app/admin/matches/page.tsx
+++ src/app/admin/matches/page.tsx
@@ -1,16 +1,14 @@
 import { CreateMatchForm } from "@/app/_components/create-match"
 import { ListMatch } from "@/app/_components/list-match"
-import { EditMatchDialog } from "./edit-match-dialog"
 
 function Page() {
-    return (
-      <div>
-          <h1>Admin Teams Matches</h1>
-          <EditMatchDialog matchId={6}></EditMatchDialog>
-          <ListMatch></ListMatch>
-          <CreateMatchForm></CreateMatchForm>
-      </div>
-    )
-  }
-  
-  export default Page
+  return (
+    <div className="mx-auto grid w-full max-w-6xl gap-2">
+      <h1 className="text-3xl font-semibold my-4">Matches</h1>
+      <ListMatch></ListMatch>
+      <CreateMatchForm></CreateMatchForm>
+    </div>
+  )
+}
+
+export default Page
