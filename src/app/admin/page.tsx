269998aae6eaa4bf716b572252588e18bce09cf4
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-03-28 12:20:19 +0100

    Admin page with tabs

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,28 @@
-import Link from 'next/link'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { CreateTeamForm } from "../_components/create-team"
+import { ListTeam } from "../_components/list-team"
+import { CreateMatchForm } from "../_components/create-match"
+import { ListMatch } from "../_components/list-match"
+
 
 function AdminPage() {
   return (
     <div className="mx-auto grid w-full max-w-6xl gap-2">
       <h1 className="text-3xl font-bold tracking-tight my-4">Admin</h1>
-      <div className='flex flex-col'>
-        <Link href="admin/teams">Teams</Link>
-        <Link href="admin/matches">Matches</Link>
-      </div>
+      <Tabs defaultValue="matches">
+        <TabsList>
+          <TabsTrigger value="matches">Matches</TabsTrigger>
+          <TabsTrigger value="teams">Teams</TabsTrigger>
+        </TabsList>
+        <TabsContent value="matches">
+          <ListMatch></ListMatch>
+          <CreateMatchForm></CreateMatchForm>
+        </TabsContent>
+        <TabsContent value="teams">
+          <ListTeam></ListTeam>
+          <CreateTeamForm></CreateTeamForm>
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
