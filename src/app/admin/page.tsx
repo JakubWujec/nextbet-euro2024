32772d4d190516dd572caa7791a7b502a7067c39
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-03-28 12:14:45 +0100

    Admin page header

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,14 +2,12 @@ import Link from 'next/link'
 
 function AdminPage() {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className="mx-auto grid w-full max-w-6xl gap-2">
+      <h1 className="text-3xl font-bold tracking-tight my-4">Admin</h1>
       <div className='flex flex-col'>
         <Link href="admin/teams">Teams</Link>
         <Link href="admin/matches">Matches</Link>
       </div>
-
-
     </div>
   )
 }
