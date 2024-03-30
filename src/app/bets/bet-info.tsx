45a3db0411dfc0b89523a8260b6234fdf85f4974
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-03-30 13:35:46 +0100

    no bet infO

--- src/app/bets/bet-info.tsx
+++ src/app/bets/bet-info.tsx
@@ -7,6 +7,8 @@ type BetInfoProps = {
 }
 
 export function BetInfo({ match }: BetInfoProps) {
+    const hasBet = match.bets.length;
+
     return (
         <div className="grid grid-cols-4 grid-rows-4 gap-2 border border-gray-300 rounded-lg overflow-hidden shadow-md">
             <div className="flex flex-col items-center justify-center row-span-4">
@@ -21,8 +23,14 @@ export function BetInfo({ match }: BetInfoProps) {
                 <p className="text-xl font-bold">2</p>
             </div>
             <div className="flex flex-col pb-4 items-center justify-center col-start-2 col-span-1 row-start-3 row-span-2">
-                <p className="text-sm font-semibold">Your bet:</p>
-                <p className="text-sm font-bold">{match.bets[0]?.homeTeamScore}:{match.bets[0]?.awayTeamScore}</p>
+                {hasBet ? <>
+                    <p className="text-sm font-semibold">Your bet:</p>
+                    <p className="text-sm font-bold">{match.bets[0]?.homeTeamScore ?? "?"}:{match.bets[0]?.awayTeamScore ?? "?"}</p>
+                </> : <>
+                    <p className="text-sm font-semibold">Your bet:</p>
+                    <p className="text-sm font-bold">No bet</p>
+                </>}
+
             </div>
             <div className="flex items-center justify-center bg-green-200 row-start-1 row-span-4 col-start-4">
                 <p className="text-3xl font-semibold">10</p>
@@ -31,3 +39,4 @@ export function BetInfo({ match }: BetInfoProps) {
         </div>
     )
 }
+
