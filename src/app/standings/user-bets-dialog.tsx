d2ef7dedf7dfdbb7ab0f353ab583324303fa1720
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-04-02 17:14:36 +0200

    removed list-user-bets

--- src/app/standings/user-bets-dialog.tsx
+++ src/app/standings/user-bets-dialog.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { UserBetList } from "./list-user-bets";
+import { api } from "@/trpc/react";
+import { BetInfo } from "../bets/bet-info";
 
 type UserBetsDialogProps = {
     userName: string;
@@ -10,11 +11,25 @@ type UserBetsDialogProps = {
 }
 
 export function UserBetsDialog({ userName, open, setOpen }: UserBetsDialogProps) {
+    const { data: matchesWithBets, isLoading } = api.match.getListWithUserBetsThatHaveStartedAlready.useQuery({ userName: userName });
+
+    if (isLoading) return <div>Loading...</div>
+    if (!matchesWithBets) return <div>Something went wrong...</div>
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent>
-                <UserBetList userName={userName}></UserBetList>
+                <div>
+                    {(!matchesWithBets.length) ? (<div> No matches this day.</div>) :
+                        <div>
+                            {matchesWithBets.map(matchWithBet =>
+                                <div key={matchWithBet.id} className="my-4">
+                                    <BetInfo match={matchWithBet}></BetInfo>
+                                </div>
+                            )}
+                        </div>
+                    }
+                </div>
             </DialogContent>
         </Dialog >
     )
