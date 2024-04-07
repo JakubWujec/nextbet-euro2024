64e4e656b9f931e624fdd904c99d7f648f782cc2
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-04-07 12:34:15 +0200

    Admin side navigation

---
+++ src/app/admin/actions/page.tsx
@@ -0,0 +1,9 @@
+import { UpdatePoints } from "./update-points";
+
+function Page() {
+    return <div className="flex items-center justify-between">
+        <UpdatePoints></UpdatePoints>
+    </div>
+}
+
+export default Page
