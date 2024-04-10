2256a763d3979b9054e681d5bedd4ea6b27db1ba
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-04-10 16:17:22 +0200

    usage of datepickerdemo inside date-carousel

--- src/components/date-carousel.tsx
+++ src/components/date-carousel.tsx
@@ -1,12 +1,14 @@
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import { DatePickerDemo } from "./ui/date-picker-demo";
+import { TimePickerDemo } from "./ui/time-picker-demo";
 
-type DateCarouselProps =  {
+type DateCarouselProps = {
     selectedDate: Date,
     setSelectedDate: (date: Date) => void
 }
 
-export function DateCarousel({selectedDate, setSelectedDate}: DateCarouselProps) {
+export function DateCarousel({ selectedDate, setSelectedDate }: DateCarouselProps) {
     const onNext = () => {
         const nextDate = new Date(selectedDate);
         nextDate.setDate(nextDate.getDate() + 1);
@@ -19,11 +21,17 @@ export function DateCarousel({selectedDate, setSelectedDate}: DateCarouselProps)
         setSelectedDate(prevDate);
     };
 
+    function setDate(date: Date | undefined) {
+        if (date) {
+            setSelectedDate(date);
+        }
+    }
+
     return (
         <Card className="flex items-center p-2">
             <Button onClick={onPrev}>Prev</Button>
-            <div>{selectedDate.toLocaleDateString()}</div>
-            <Button onClick={onNext}>Next</Button>            
+            <DatePickerDemo date={selectedDate} setDate={setDate}></DatePickerDemo>
+            <Button onClick={onNext}>Next</Button>
 
         </Card>
     )
