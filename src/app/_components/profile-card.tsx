a4f843cb62f46dad917fa28e67d2c2001adbd6e0
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-04-18 19:51:37 +0200

    endpoint for user stats

--- src/app/_components/profile-card.tsx
+++ src/app/_components/profile-card.tsx
@@ -1,30 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { api } from "@/trpc/react";
 import Link from "next/link";
 
-const username = 'Adrax_Sazir'
-const usernameSub = "AS24"
-const winRate = 20
-const betsCompleted = 40
-const betsPending = 10
-const points = 36
 
 type ProfileCardProps = {
     name: string;
 }
 
 export function ProfileCard({ name }: ProfileCardProps) {
+    const { data, isLoading } = api.user.getUserStats.useQuery({ userName: name });
+
+    if (!data) return null;
+
     return (
         <Card>
             <CardHeader className="flex flex-col items-center">
                 <CardTitle>{name}</CardTitle>
-                <CardDescription>{usernameSub}</CardDescription>
+                <CardDescription>{name}</CardDescription>
             </CardHeader>
             <CardContent className="flex justify-center flex-wrap gap-2">
-                <ScoreCard title="Points" value={`${points}`}></ScoreCard>
-                <ScoreCard title="Completed" value={`${betsCompleted}`}></ScoreCard>
-                <ScoreCard title="Completed" value={`${betsCompleted}`}></ScoreCard>
-                <ScoreCard title="Completed" value={`${betsCompleted}`}></ScoreCard>
+                <ScoreCard title="Bets" value={`${data.count}`}></ScoreCard>
+                <ScoreCard title="Points" value={`${data.points}`}></ScoreCard>
+                <ScoreCard title="Winners" value={`${data.correctWinner}`}></ScoreCard>
+                <ScoreCard title="Scores" value={`${data.correctScore}`}></ScoreCard>
             </CardContent>
             <CardFooter className="flex justify-center">
                 <Button asChild className="w-full">
@@ -44,12 +45,11 @@ function ScoreCard({ title, value }: ScoreCardProps) {
     return (
         <Card className="w-24 aspect-square">
             <CardHeader className="flex flex-col items-center ">
-                <CardTitle className="text-md">{title}</CardTitle>
+                <CardTitle className="text-md whitespace-nowrap">{title}</CardTitle>
             </CardHeader>
             <CardContent className="flex items-center justify-center font-bold text-2xl">
                 <p>{value}</p>
             </CardContent>
-
         </Card>
     )
 }
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -41,4 +41,40 @@ export const userRouter = createTRPCRouter({
         .query(({ ctx }) => {
             return ctx.db.team.findMany();
         }),
+
+    getUserStats: publicProcedure
+        .input(z.object({ userName: z.string() }))
+        .query(async ({ ctx, input }) => {
+            const findUser = await ctx.db.user.findFirst({
+                where: {
+                    name: input.userName
+                }
+            });
+            if (!findUser) {
+                throw new Error("User not exist")
+            }
+
+            const queryResult = await ctx.db.bet.groupBy({
+                by: ['points'],
+                _count: {
+                    _all: true,
+                },
+                where: {
+                    userId: findUser.id
+                }
+            })
+
+            let count = queryResult.reduce((a, b) => a + b._count._all, 0);
+            let points = queryResult.reduce((a, b) => a + b.points * b._count._all, 0);
+            let correctWinner = queryResult.find(x => x.points === 2)?._count._all ?? 0;
+            let correctScore = queryResult.find(x => x.points === 5)?._count._all ?? 0;
+
+
+            return {
+                points: points,
+                count: count,
+                correctScore: correctScore,
+                correctWinner: correctWinner
+            }
+        }),
 });
