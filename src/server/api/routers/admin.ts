43225835bedf146e0024a2fae2fde21838b4693e
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-04-26 13:26:46 +0200

    remove unused endpoint

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -12,27 +12,4 @@ export const adminRouter = createTRPCRouter({
     await calculatePointsQuery();
     await updateLastStandingsUpdateDate();
   }),
-
-  updateBetPointsByMatchId: protectedProcedure
-    .input(z.object({ matchId: z.number() }))
-    .mutation(async ({ ctx, input }) => {
-      // for every bet on this match update points column;
-      const result = await ctx.db.$queryRaw`
-            UPDATE "Bet"
-              set "points" = 
-                (CASE 
-                when "Match"."homeTeamScore" = "Bet"."homeTeamScore" 
-                AND "Match"."awayTeamScore" = "Bet"."awayTeamScore" 
-                THEN 5
-                when sign("Match"."homeTeamScore" - "Match"."awayTeamScore") = sign("Bet"."homeTeamScore" - "Bet"."awayTeamScore")
-                THEN 2
-                else 0 END)
-            FROM 
-              "Bet" as "BetAlias"
-              INNER JOIN 
-              "Match" ON "Match"."id" = "BetAlias"."matchId" AND "BetAlias"."matchId" = ${input.matchId}
-            WHERE "Bet"."matchId" = ${input.matchId}
-            `
-      return result;
-    }),
 });
